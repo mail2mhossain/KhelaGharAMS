fef1a270330621ada5ea5465de0f6996c3c8fa45
apl <apl@DESKTOP-TRK4SPA>
2017-09-21 12:28:05 +0600

    API for Apps has been implemented

---
+++ trunk/source/KhelaGharAMS/src/Site/KhelaGharAmsApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace KhelaGharAmsApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ trunk/source/KhelaGharAMS/src/Site/KhelaGharAmsApi/Controllers/AsarController.cs
@@ -0,0 +1,47 @@
+﻿using KhelaGhar.AMS.Model.DbAccess;
+using KhelaGhar.AMS.Model.Repository;
+using KhelaGharAmsApi.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using KhelaGhar.AMS.Model.Domain.Asars;
+
+namespace KhelaGharAmsApi.Controllers
+{
+  public class AsarController : ApiController
+  {
+    [HttpGet]
+    [Route("appsapi/Asar")]
+    public IHttpActionResult GetAsar(string name)
+    {
+      IList<AsarInfo> asarList = new List<AsarInfo>();
+      using (KhelaGharAMSDbContext dbContext = new KhelaGharAMSDbContext())
+      {
+        ApiRepository repo = new ApiRepository(dbContext);
+        asarList = MapAsar(repo.GetAsarByName(name));
+        return Content(HttpStatusCode.Found, asarList);
+      }
+    }
+
+    private IList<AsarInfo> MapAsar(IList<Asar> list)
+    {
+      IList<AsarInfo> asarList = new List<AsarInfo>();
+      foreach(ShakhaAsar asar in list)
+      {
+        AsarInfo info = new AsarInfo();
+        info.AsarName = asar.Name;
+        info.CommitteeType = asar.CommitteeType.ToString();
+        info.AsarStatus = asar.AsarStatus.ToString();
+        info.AddressLine = asar.AddressLine;
+        info.Subdistrict = asar.Area.Name;
+        info.District = asar.Area.Parent.Name;
+
+        asarList.Add(info);
+      }
+      return asarList;
+    }
+  }
+}
