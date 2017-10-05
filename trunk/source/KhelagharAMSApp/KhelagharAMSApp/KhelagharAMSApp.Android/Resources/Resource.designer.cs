a2b55467beab816389a88ce948060ca2329e06c6
apl <apl@DESKTOP-TRK4SPA>
2017-10-05 16:55:52 +0600

    login view has been updated

--- trunk/source/KhelagharAMSApp/KhelagharAMSApp/KhelagharAMSApp.Android/Resources/Resource.designer.cs
+++ trunk/source/KhelagharAMSApp/KhelagharAMSApp/KhelagharAMSApp.Android/Resources/Resource.designer.cs
@@ -72,62 +72,65 @@ namespace KhelagharAMSApp.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060010
-			public const int AsarNameEntry = 2131099664;
-			
-			// aapt resource value: 0x7f06000f
-			public const int AsarNameLabel = 2131099663;
-			
-			// aapt resource value: 0x7f06000a
-			public const int AsarNameSearchLabel = 2131099658;
-			
 			// aapt resource value: 0x7f060011
-			public const int AsarSearchBtn = 2131099665;
+			public const int AsarNameEntry = 2131099665;
+			
+			// aapt resource value: 0x7f060010
+			public const int AsarNameLabel = 2131099664;
+			
+			// aapt resource value: 0x7f06000b
+			public const int AsarNameSearchLabel = 2131099659;
+			
+			// aapt resource value: 0x7f060012
+			public const int AsarSearchBtn = 2131099666;
+			
+			// aapt resource value: 0x7f060009
+			public const int LogInButton = 2131099657;
 			
 			// aapt resource value: 0x7f060008
-			public const int LogInButton = 2131099656;
+			public const int RelativeLayoutButton = 2131099656;
 			
 			// aapt resource value: 0x7f060007
-			public const int RelativeLayoutButton = 2131099655;
+			public const int TextErrorMessage = 2131099655;
 			
 			// aapt resource value: 0x7f060006
-			public const int TextErrorMessage = 2131099654;
+			public const int TextPassword = 2131099654;
 			
-			// aapt resource value: 0x7f060005
-			public const int TextPassword = 2131099653;
-			
-			// aapt resource value: 0x7f060003
-			public const int TextUsername = 2131099651;
+			// aapt resource value: 0x7f060004
+			public const int TextUsername = 2131099652;
 			
 			// aapt resource value: 0x7f060000
 			public const int img_kg_logo = 2131099648;
 			
+			// aapt resource value: 0x7f060002
+			public const int img_khelaghar = 2131099650;
+			
 			// aapt resource value: 0x7f060001
 			public const int kg_text = 2131099649;
 			
-			// aapt resource value: 0x7f060012
-			public const int listView = 2131099666;
-			
-			// aapt resource value: 0x7f06000c
-			public const int radio_asar = 2131099660;
-			
-			// aapt resource value: 0x7f06000e
-			public const int radio_jela = 2131099662;
-			
-			// aapt resource value: 0x7f06000b
-			public const int radio_search_group = 2131099659;
+			// aapt resource value: 0x7f060013
+			public const int listView = 2131099667;
 			
 			// aapt resource value: 0x7f06000d
-			public const int radio_upojela = 2131099661;
+			public const int radio_asar = 2131099661;
 			
-			// aapt resource value: 0x7f060009
-			public const int relativeLayout1 = 2131099657;
+			// aapt resource value: 0x7f06000f
+			public const int radio_jela = 2131099663;
 			
-			// aapt resource value: 0x7f060004
-			public const int relativeLayoutPassword = 2131099652;
+			// aapt resource value: 0x7f06000c
+			public const int radio_search_group = 2131099660;
 			
-			// aapt resource value: 0x7f060002
-			public const int relativeLayoutUsername = 2131099650;
+			// aapt resource value: 0x7f06000e
+			public const int radio_upojela = 2131099662;
+			
+			// aapt resource value: 0x7f06000a
+			public const int relativeLayout1 = 2131099658;
+			
+			// aapt resource value: 0x7f060005
+			public const int relativeLayoutPassword = 2131099653;
+			
+			// aapt resource value: 0x7f060003
+			public const int relativeLayoutUsername = 2131099651;
 			
 			static Id()
 			{
