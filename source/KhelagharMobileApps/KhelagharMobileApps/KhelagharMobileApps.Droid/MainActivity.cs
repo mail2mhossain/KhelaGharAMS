5e12064d89ff2d46946dc4cd56cef0c296d8294d
apl <apl@DESKTOP-TRK4SPA>
2017-10-10 13:54:56 +0600

    SplashScreen has been implemented in Android of KhelagharMobileApps

--- source/KhelagharMobileApps/KhelagharMobileApps/KhelagharMobileApps.Droid/MainActivity.cs
+++ source/KhelagharMobileApps/KhelagharMobileApps/KhelagharMobileApps.Droid/MainActivity.cs
@@ -11,7 +11,7 @@ using Prism.DryIoc;
 
 namespace KhelagharMobileApps.Droid
 {
-  [Activity(Label = "KhelagharMobileApps", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+  [Activity(Label = "KhelagharMobileApps", Icon = "@drawable/icon", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
   public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
   {
     protected override void OnCreate(Bundle bundle)
